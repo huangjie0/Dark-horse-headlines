7f5d4290154b86acb67017dd4eed542943dbeafc
huangjie <1364045672@qq.com>
2022-07-17 21:44:59 +0800

    youhua

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,20 @@
 
 <script>
 import { getAllChannelsAPI } from '@/api/index'
+// try catch来来捕获同步代码的错误
 export default {
   async created () {
-    const res = await getAllChannelsAPI()
-    console.log(res)
+    try {
+      const res = await getAllChannelsAPI()
+      console.log(res)
+    } catch (error) {
+      // err参数拿到就是错误对象
+      // 详细打印
+      console.dir(error)
+    }
   }
 }
 </script>
-
 <style>
 
 </style>
