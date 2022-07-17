fb0c6a4733680e849aefc235d2b7a2ee87ff459c
huangjie <1364045672@qq.com>
2022-07-17 23:05:08 +0800

    youhua

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'amfe-flexible' // 用来设置根标签字体大小（移动端适配）
-import { NavBar } from 'vant'
+import { NavBar, Form, Field } from 'vant'
 
 Vue.use(NavBar)
+Vue.use(Form)
+Vue.use(Field)
 
 Vue.config.productionTip = false
 
